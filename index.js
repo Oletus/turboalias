0b9d356da6067cc91d1555fe787be2b39c0df6bb
Olli Etuaho <olli.etuaho@gmail.com>
2020-03-31 23:02:49 +0300

    Don't allow duplicates where only the case is different

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.raw());
 const port = 80;
 
 let collectedStrings = [];
+let collectedStringsLowercase = [];
 
 let currentRoundStrings = [];
 
@@ -20,22 +21,24 @@ app.get('/', (req, res) => {
   <input type="submit" value="Ehdota" />
 </form>
 <form method="POST" action="/start-round">
-  <input type="submit" value="Aloita kierros" />
+  <input type="submit" value="Laita kerätyt sanat hattuun" />
 </form>
 <form method="POST" action="/random-word">
   <input type="submit" value="Poimi sana hatusta" />
 </form>
 `)
-})
+});
 
 app.post('/submit-string', (req, res) => {
   console.log('Got item:', req.body);
   if (req.body.input !== undefined && req.body.input.trim() !== "") {
     const string = req.body.input.trim();
-    if (collectedStrings.indexOf(string) >= 0) {
+    const stringLower = string.toLowerCase();
+    if (collectedStringsLowercase.indexOf(stringLower) >= 0) {
       res.send('<span style="color: red">Nyt tais tulla sama sana tuplana.</span><br><a href="/">koitapa uusiksi</a>');
     } else {
       collectedStrings.push(string);
+      collectedStringsLowercase.push(stringLower);
       res.send('Vastaanotettiin: "' + req.body.input + '".<br/> Yhteensä kerättynä sanoja: ' + collectedStrings.length + '<br><a href="/">takas</a>');
     }
   } else {
@@ -45,7 +48,7 @@ app.post('/submit-string', (req, res) => {
 
 app.post('/start-round', (req, res) => {
   currentRoundStrings = [...collectedStrings];
-  res.send('Kierros aloitettu!' + '<br><a href="/">takas</a>');
+  res.send('Sanat on kaikki hatussa, kierros on valmiina alkamaan!' + '<br><a href="/">takas</a>');
 });
 
 app.post('/random-word', (req, res) => {
@@ -70,4 +73,4 @@ app.listen(port, (err) => {
   }
 
   console.log(`server is listening on ${port}`)
-})
+});
